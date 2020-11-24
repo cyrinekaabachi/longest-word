a4d7deeca62a0e4ebe5a15ab7173a2f0da1feb29
cyrinekaabachi <kaabachi.cyrine@gmail.com>
2020-11-24 16:42:28 +0100

    added api

--- game.py
+++ game.py
@@ -3,12 +3,19 @@
 
 import string
 import random
+import requests
 
 class Game:
     def __init__(self):
         self.grid = []
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
 
     def is_valid(self, word):
         if not word:
@@ -19,4 +26,4 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return True and __check_dictionary(word)
