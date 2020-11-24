b987a5e1417d143ffd6b8534ad976137e03796e1
cyrinekaabachi <kaabachi.cyrine@gmail.com>
2020-11-24 15:41:35 +0100

    Game development with TDD

---
+++ tests/test_game.py
@@ -0,0 +1,23 @@
+# tests/test_game.py
+import unittest
+import string
+from game import Game
+
+class TestGame(unittest.TestCase):
+    def test_game_initialization(self):
+        new_game = Game()
+        grid = new_game.grid
+        self.assertIsInstance(grid, list)
+        self.assertEqual(len(grid), 9)
+        for letter in grid:
+            self.assertIn(letter, string.ascii_uppercase)
+
+    def test_is_valid(self):
+        new_game = Game()
+        new_game.grid = list('KWEUEAKRZ')
+        self.assertIs(new_game.is_valid('EUREKA'), True) # assertIs tests for object identity (is / is not)
+        self.assertEqual(new_game.grid, list('KWEUEAKRZ')) # tests for equality like the == operator
+
+    
+
+
